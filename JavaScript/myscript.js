eac9f80becee4cf274e88c1dedf85a411941ce20
Gautam <pandeygautam394@gmail.com>
2025-10-11 14:43:15 +0530

    Day 01 of JavaScript

---
+++ JavaScript/myscript.js
@@ -0,0 +1,18 @@
+alert("Hello MRG")
+console.log("This is the first Console log")
+
+var a=prompt("Enter a number")
+var p=confirm("Can i destory your devices")
+
+if(p) {
+    console.log("Your Devics is going to destroy")
+} else {
+    console.log("Your device is not destroying")
+}
+var t=prompt("Enter a Number")
+if(t%2==0){
+    console.log("Your number is even")
+} else {
+    console.log("Your Number is not even Your number is old")
+}
+console.log("Done")
---
+++ JavaScript/newscript.js
@@ -0,0 +1,5 @@
+console.log("hello World")
+for(let i=0;i<10;i++) {
+    console.log("MRG "+"00"+i);
+}
+console.log("Your for loop is completed");
