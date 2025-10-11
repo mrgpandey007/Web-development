c547abd4d0a2f5cf17fff732e80333b76c7915eb
Gautam <pandeygautam394@gmail.com>
2025-10-11 17:23:03 +0530

    Conditional and looping statement done in JavaScript

---
+++ JavaScript/Day2/Conditional.js
@@ -0,0 +1,21 @@
+console.log("Conditional Statement in javaScript")
+let x=48
+if(x>18){
+     console.log("you can drive")
+} else {
+    console.log("You cannot drive")
+}
+
+let b=0
+if(b>18){
+     console.log("you can drive")
+} else if(b==0) {
+    console.log("No.....................!")
+} else {
+    console.log("you cannot drive")
+}
+
+let a=10
+let c=20
+console.log("Maximum number is\t:"+(a>c?a:c))
+console.log("Minimum number is\t:"+(a<c?a:c))
---
+++ JavaScript/Day2/loopingstatement.js
@@ -0,0 +1,25 @@
+/*for loop
+for(let x=0;x<100;x++) {
+    console.log("  X->\t:"+(x+1))
+}*/
+
+/* for in loop
+let obj={
+    name:"MRG",
+    age:21,
+    Address:"India"
+}
+for(const i in obj) {
+    console.log("Key "+i+"\tValue "+obj[i])
+}
+
+let d=10;
+while(d>0) {
+    console.log(d--);
+}*/
+
+let e=25;
+do{
+    console.log(e);
+    e--;
+}while(e>0);
